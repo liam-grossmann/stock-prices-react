81b09141793ca605f1e0eb8d945a171d4b7b8e90
Liam Grossmann <liam.grossmann@protonmail.com>
2022-10-24 11:18:28 +0200

    Tidy code. Comment use of hooks

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import WatchList from './components/watchList'
 export default function App() {
   return (
     <div className="App">
-      <div> <h3>Stock Price Simulator (ReactJS, D3.js and Simple Timer)</h3></div>
+      <div> <h3>Stock Price Simulator (ReactJS, D3.js and some advanced use of react hooks)</h3></div>
       <br/>
       <div>Select a ticker on the left hand list to display the details and chart for the selected ticker</div>
       <br/>
